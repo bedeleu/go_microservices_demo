4a1e2e1a6dd8e9ffd67ede5b408c5a23311fdfd1
Bedeleu Alexandru <alex.bedeleu@gmail.com>
2024-12-09 12:32:44 +0200

    Middleware added

--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/bedeleu/go_microservices_demo.git
-
-go 1.23.4
-
-require github.com/go-chi/chi/v5 v5.1.0
--- main.go
+++ main.go
@@ -2,19 +2,30 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
 )
 
 func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	// Adaugă ruta pentru handler
+	router.Get("/hello", basicHandler)
+
 	server := &http.Server{
 		Addr:    ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
+
+	fmt.Println("Server starting on port 3000...")
 	err := server.ListenAndServe()
 	if err != nil {
-		fmt.Println("Failed to listen to servere", err)
+		fmt.Println("Failed to listen to server:", err)
 	}
 }
+
 func basicHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello World"))
 }
