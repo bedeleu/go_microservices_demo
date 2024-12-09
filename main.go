f244ba405f81feb91076227fd570247795251de7
Bedeleu Alexandru <alex.bedeleu@gmail.com>
2024-12-09 12:44:38 +0200

    Middleware added

--- main.go
+++ main.go
@@ -1,31 +1,11 @@
 package main
 
 import (
+	""
+	"context"
 	"fmt"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"net/http"
 )
 
 func main() {
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
 
-	// Adaugă ruta pentru handler
-	router.Get("/hello", basicHandler)
-
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: router,
-	}
-
-	fmt.Println("Server starting on port 3000...")
-	err := server.ListenAndServe()
-	if err != nil {
-		fmt.Println("Failed to listen to server:", err)
-	}
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
 }
